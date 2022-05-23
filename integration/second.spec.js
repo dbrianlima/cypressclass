adcae33f4def52a033f332401a1183b226426298
David Brian <dbrianlima@gmail.com>
2022-05-23 17:35:42 -0300

    Ajustes

--- integration/second.spec.js
+++ integration/second.spec.js
@@ -1,17 +1,25 @@
 ///<reference types="cypress" /> 
 
-describe('cadastro_ficha', () =>{
+describe('acesso_login', () =>{
 
-    it('colocar no carrinho', () => {
+    it.only('colocar no carrinho', () => {
 
-        cy.visit('http://practice.automationtesting.in/shop/')
+        cy.visit('https://www.saucedemo.com/')
 
-        cy.get('[data-product_id="169"]').click()
-        
+        cy.get('[id="user-name"]').type('standard_user');
+        cy.get('[id="password"]').type('secret_sauce');
+        cy.get('[id="login-button"]').click();
+        cy.get('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]').click();
+        cy.get('.shopping_cart_link').click();
 
     })
 
+    it('colocar no carrinho', () => {
+        cy.visit('https://www.saucedemo.com/inventory.html')
 
+
+
+    })
 
 
 })
--- integration/tstform.spec.js
+++ integration/tstform.spec.js
@@ -8,31 +8,25 @@ describe('buton test', () => {
         
         cy.visit("http://demo.automationtesting.in/Register.html");
 
-        cy.get('[placeholder="First Name"]').type("Test")
-        cy.get('[placeholder="Last Name"]').type("Last Name")
+        cy.get('[placeholder="First Name"]').type("Test");
+        cy.get('[placeholder="Last Name"]').type("Last Name");
         cy.get('[ng-model="Adress"]').type("Rua Teste, 1234");
-        cy.get('[type="email"]').type("Email@test.com")
-        cy.get('[type="tel"]').type("5582987248299")
+        cy.get('[type="email"]').type("Email@test.com");
+        cy.get('[type="tel"]').type("5582987248299");
         cy.get('[value="FeMale"]').click();
         cy.get('[id="checkbox1"]').click();
         cy.get('[id="checkbox2"]').click();
         cy.get('[id="checkbox3"]').click();
-        //cy.get('[id="msdd"]').select('Arabic').should('have.value', 'Arabic');
         cy.get('[id="Skills"]').select('Ruby').should('have.value', 'Ruby');
-        cy.get('[id="countries"]').select('Select Country');
-        //cy.get('[id="select2-country-container"]').click().type("Australia");
+        cy.get('[id="countries"]').select('Select Country');   
         cy.get('[id="yearbox"]').select('1916');
         cy.get('[placeholder="Month"]').select('September');
         cy.get('[id="daybox"]').select('8');
         cy.get('[id="firstpassword"]').type("123456");
-        cy.get('[id="secondpassword"]').type("123456")
+        cy.get('[id="secondpassword"]').type("123456");
         cy.get('[id="submitbtn"]').click();
-        //cy.get('[id="imagesrc"]').click();
-        //cy.get('[id="Button1"]').click();
         
-
-
-
+    
 
 
     });
