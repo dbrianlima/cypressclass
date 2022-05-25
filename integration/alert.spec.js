ed484dc333f7befd6c33ee878f4d1d6145e53239
David Brian <dbrianlima@gmail.com>
2022-05-25 18:57:01 -0300

    Alert test
    Test with alert, Alert cconfirmation and Prompt messeger

--- integration/alert.spec.js
+++ integration/alert.spec.js
@@ -1,20 +1,47 @@
 ///<reference types="cypress" /> 
 
+
+
+
 describe("teste alert", () => {
 
-
+    
+    beforeEach(() => {
+        cy.visit('http://the-internet.herokuapp.com/javascript_alerts');
+    });
+    
     it("alerta_click_ok", () => {
-
-        cy.visit('http://demo.automationtesting.in/Alerts.html');
-
-        cy.get('.active > .analystic').click();
-        cy.get('#OKTab > .btn').click();
-
-
+        //Cypress trata de forma virtua o Alerta(por isso não é exibido no Browser)
+        cy.get('[onclick="jsAlert()"]').click();
+        cy.on('window:alert',(text) => {
+            expect(text).to.contain('I am a JS Alert')
+        });
+        cy.get('#result').contains('You successfully clicked an alert');
 
     });
+    
+    it('confirm alert', () =>{
+        cy.get(':nth-child(2) > button').click();
+
+        cy.on('window.confirm' , (text) => {
+            expect(text).to.contain('I am a JS Confirm')
+        });
+        cy.get('#result').contains('You clicked: Ok');
+    });
+    
 
 
+    it('prompt message', () => {
 
+        cy.window().then(win => {
+            cy.stub(win, 'prompt').returns('Test');
+            cy.get('[onclick="jsPrompt()"]').click();
+            //cy.get('#prompt-answer').contains('Answer: Test');
+
+            cy.get('[id="result"]').contains('You entered: Test')
+        })
+
+
+    })
 
 });
